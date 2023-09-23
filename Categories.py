10518f2f3a74191572b7e9532378dacfcb6ad037
Kirill Kudinov <Dr.kireal@yandex.ru>
2023-09-23 05:40:23 +0300

    1,2,3,4,5

--- Categories.py
+++ Categories.py
@@ -1,7 +1,10 @@
 import pytest
 import requests
 import json
-from settings import login, user, url_category, user_id, url_category_id
+from settings import login, user, url_category, user_id, user_new,url_category_new
+from colorama import init, Fore
+
+init(autoreset=True)
 
 
 # structirize json file
@@ -25,30 +28,71 @@ jprint(r.json())
 
 # 2 Create Category
 def create_new_category():
+    print(Fore.CYAN + "create_new_category() is running now")
     r = requests.post(url_category, auth=login, data=user)
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        print('ERROR: %s' % e)
-    assert r.status_code != 201
+        print(Fore.CYAN + 'ERROR: %s' % e)
+
     if(r.status_code == 201):
-        print("Pet Created")
-    else:
-        print("Current status is: " f'{r.status_code}.', "Category already exists, type another type of category")
+        assert r.status_code == 201
+        print(r.status_code)
+        print(Fore.GREEN + "Function Create_new_category succeeded, Pet Created")
+    elif(r.status_code != 201):
+        print(Fore.BLUE + "Function Create_new_category failed. Current status is: " f'{r.status_code}.', Fore.BLUE+ "Category already exists, "
+                                                                                    "enter another type of category")
+    print(Fore.CYAN + "Function create_new_category() ends ")
 
 
-create_new_category()
 
 
+
+# 3
 def get_status_of_category_by_id():
+    print(Fore.CYAN + "get_status_of_category_by_id() is running now")
     number = input('Enter id number:')
     r = requests.get(url_category + str(number), auth=login)
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        print("ERROR this id " f'{number}' " does not exist: %s" % e)
+        print(Fore.CYAN + "ERROR this id " f'{number}' " does not exist: %s" % e)
     print(r.json())
+    print(Fore.CYAN + "Function get_status_of_category_by_id() ends")
+
+# 4
+def put_category_by_id():
+    print(Fore.CYAN + "put_category_by_id() is running now")
+    r = requests.put(url_category_new, auth=login, json=user_new)
+
+    try:
+      r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+     print('ERROR: %s' % e)
+     assert r.status_code == 201
+    if (r.status_code == 200):
+        print("Category Updated")
+    else:
+        print("Current status is: " f'{r.status_code}.', "Category already exists, type another type of category")
+    print(r.json())
+    print(Fore.CYAN + "Function put_category_by_id() ends")
 
 
-get_status_of_category_by_id()
 
+
+# 5
+def delete_category_by_id():
+    print(Fore.CYAN +"Function delete_category_by_id() is running now")
+    number = input(Fore.GREEN + 'Which id number do you want to delete:')
+    r = requests.delete(url_category + str(number), auth=login)
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(Fore.YELLOW + "ERROR this id " f'{number}' " does not exist anymore or it never exist before: %s" % e)
+    if(r.status_code == 204):
+        print(Fore.GREEN + "You've delete line successfully, status code: "f'{r.status_code}')
+
+    else:
+        print(Fore.YELLOW + "Line does not exist, status code:"f'{r.status_code}')
+
+    print(Fore.CYAN + "Function Function delete_category_by_id() ends")
--- settings.py
+++ settings.py
@@ -3,12 +3,21 @@ from requests.auth import HTTPBasicAuth
 
 login = HTTPBasicAuth('admin', 'admin')
 user = {
-        "name": "5  "
+
+        "name": "15"
     }
 
 url_category = "http://91.210.171.73:8080/api/category/"
 user_id = {
-  "id": 217,
-  "name": "string"
+
+  "name": "back"
 }
 
+user_new = {
+
+
+  "name": "16"
+}
+
+
+url_category_new = "http://91.210.171.73:8080/api/category/230/"
